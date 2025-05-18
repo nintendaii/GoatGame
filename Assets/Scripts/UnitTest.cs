f3e510dc3ce46a59b92f3e94630a15fe54c825f4
Daniel <44233090+nintendaii@users.noreply.github.com>
2025-05-18 22:25:38 +0300

    Refactoring pt2

--- Assets/Scripts/UnitTest.cs
+++ Assets/Scripts/UnitTest.cs
@@ -1,13 +1,13 @@
 using System;
 using Data;
 using Data.Items;
+using Data.Stats;
 using UnityEngine;
 
 namespace DefaultNamespace
 {
-    public class UnitTest: MonoBehaviour
+    public class UnitTest : MonoBehaviour
     {
         [SerializeField] public UnitEntityStats unitEntityStats;
-        
     }
 }
