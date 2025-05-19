913c27afbb10e25d2b494c2999cbf511e736f68b
Daniel <44233090+nintendaii@users.noreply.github.com>
2025-05-19 16:13:46 +0300

    Added damage & resistance stats info

--- Assets/Scripts/UI/UnitItemUI.cs
+++ Assets/Scripts/UI/UnitItemUI.cs
@@ -13,6 +13,8 @@ namespace UI
         [SerializeField] private Button targetButton;
         [SerializeField] private TMP_Text unitName;
         [SerializeField] private TMP_Text healthText;
+        [SerializeField] private TMP_Text damageInfoText;
+        [SerializeField] private TMP_Text resistanceInfoText;
 
         public event Action<string> OnTargetSelected; 
         private float maxHealth;
@@ -40,8 +42,28 @@ namespace UI
             unitName.text = _unitEntityController.unitEntityData.Name;
             unitAvatarImage.sprite = unit.UnitAvatarSprite;
             SetHealth(unit.unitEntityData.CoreStats.Health.Value);
+            SetDamageInfo();
+            SetResistanceInfo();
         }
-        
+
+        private void SetResistanceInfo()
+        {
+            var s = "";
+            s += $"ARM: {_unitEntityController.unitEntityData.CoreStats.Armor.Value}\n";
+            s += $"FIR: {_unitEntityController.unitEntityData.CoreStats.Resistances.Fire.Value}\n";
+            s += $"ICE: {_unitEntityController.unitEntityData.CoreStats.Resistances.Ice.Value}\n";
+            s += $"DAR: {_unitEntityController.unitEntityData.CoreStats.Resistances.Dark.Value}\n";
+            s += $"LIT: {_unitEntityController.unitEntityData.CoreStats.Resistances.Lightning.Value}\n";
+            resistanceInfoText.text = s;
+        }
+
+        private void SetDamageInfo()
+        {
+            var s = "";
+            s += $"PHY: {_unitEntityController.unitEntityData.CoreStats.PhysicalDamage.Value}";
+            damageInfoText.text = s;
+        }
+
         public void SetHealth(float value)
         {
             healthBarFillImage.fillAmount = Mathf.Clamp01(value / maxHealth);
